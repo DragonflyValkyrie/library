2efe7bf93c933123cf62de4f03cccf6ae8a1c435
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2023-12-13 14:15:55 -0500

    Add header and footer with link, form for new book

--- script.js
+++ script.js
@@ -52,8 +52,32 @@ function displayBooks() {
     }
 }
 
-
 addBookToLibrary("The Hobbit", "J.R.R Tolkien", 255, "Yes");
 addBookToLibrary("Lord Of The Rings", "J.R.R Tolkien", 3785, "No");
 
 displayBooks();
+
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+
+openButton.addEventListener("click", () => {
+    modal.showModal();
+});
+
+// closeButton.addEventListener("click", () => {
+//     modal.close();
+// });
+
+modal.addEventListener("click", e => {
+    const dialogDimensions = modal.getBoundingClientRect();
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close();
+    }
+});
+
