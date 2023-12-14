c4fb10b325986e7127d8915df212e97b74582978
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2023-12-13 20:34:47 -0500

    Logic added if book has been read
    Refactor code for improved readability, added comments and,
    standardize styling.

--- script.js
+++ script.js
@@ -1,58 +1,73 @@
 const myLibrary = [];
 
-//Book constructor
+// Book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
+    this.info = function () {
         return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
     };
 }
 
-//Add new book to library
+// Book prototype method to toggle read status
+Book.prototype.readBook = function () {
+    // Toggle read status between "Yes" and "No"
+    this.read = this.read === "No" ? "Yes" : "No";
+};
+
+// Add new book to library
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-//Display the books in card format
+// Display the books in card format
 function displayBooks() {
-    const container = document.getElementById('book-container');
-    container.innerHTML = '';
+    const container = document.getElementById("book-container");
+    container.innerHTML = "";
 
     for (let i = 0; i < myLibrary.length; i++) {
         const book = myLibrary[i];
 
         // Create a div element for each book
-        const bookCard = document.createElement('div');
-        bookCard.classList.add('card');
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("card");
 
         // Create separate elements for each piece of information
-        const titleElement = document.createElement('h2');
+        const titleElement = document.createElement("h2");
         titleElement.textContent = `Title: ${book.title}`;
 
-        const authorElement = document.createElement('p');
+        const authorElement = document.createElement("p");
         authorElement.textContent = `Author: ${book.author}`;
 
-        const pagesElement = document.createElement('p');
+        const pagesElement = document.createElement("p");
         pagesElement.textContent = `Pages: ${book.pages}`;
 
-        const readElement = document.createElement('p');
+        const readElement = document.createElement("p");
         readElement.textContent = `Read: ${book.read}`;
 
-        const buttonReadElement = document.createElement('button');
-        buttonReadElement.textContent = 'Finished Reading';
+        const readBook = document.createElement("button");
+        readBook.textContent = "Have Read";
+        readBook.dataset.index = i;
 
-        const removeElement = document.createElement('button');
-        removeElement.textContent = 'Remove Book';
-        removeElement.dataset.index = i; 
+        const removeBook = document.createElement("button");
+        removeBook.textContent = "Remove Book";
+        removeBook.dataset.index = i;
 
-        removeElement.addEventListener('click', function() {
-            const indexToRemove = this.dataset.index; // Get the index from the data attribute
+        // Updates books that have been or not read
+        readBook.addEventListener("click", function () {
+            const indexHasBeenRead = this.dataset.index;
+            myLibrary[indexHasBeenRead].readBook();
+            displayBooks();
+        });
+
+        // Removes book from library
+        removeBook.addEventListener("click", function () {
+            const indexToRemove = this.dataset.index;
             removeBookFromLibrary(indexToRemove);
-            displayBooks(); // Update the display after removing the book
+            displayBooks();
         });
 
         // Append the elements to the card
@@ -60,42 +75,47 @@ function displayBooks() {
         bookCard.appendChild(authorElement);
         bookCard.appendChild(pagesElement);
         bookCard.appendChild(readElement);
-        bookCard.appendChild(buttonReadElement);
-        bookCard.appendChild(removeElement);
+        bookCard.appendChild(readBook);
+        bookCard.appendChild(removeBook);
 
         // Append the card to the container
         container.appendChild(bookCard);
     }
 }
 
+// Add sample books to the library
 addBookToLibrary("The Hobbit", "J.R.R Tolkien", 255, "Yes");
 addBookToLibrary("Lord Of The Rings", "J.R.R Tolkien", 3785, "No");
 
+// Display the initial set of books
 displayBooks();
 
-
+// Function to remove a book from the library
 function removeBookFromLibrary(index) {
     myLibrary.splice(index, 1);
 }
 
-//Listen For Form Sumbit Button
-document.getElementById('bookForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevents the default form submission behavior
+// Listen for form submit button
+document.getElementById("bookForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
 
     // Retrieve form values
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-    const read = document.getElementById('read').checked ? "Yes" : "No";
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked ? "Yes" : "No";
 
-    addBookToLibrary(title, author, pages, read)
-    event.target.reset(); // Resets the form
+    // Add the new book to the library
+    addBookToLibrary(title, author, pages, read);
 
+    // Reset the form
+    event.target.reset();
+
+    // Update and display the books
     displayBooks();
-    
 });
 
-
+// Modal functionality
 const openButton = document.querySelector("[data-open-modal]");
 const closeButton = document.querySelector("[data-close-modal]");
 const modal = document.querySelector("[data-modal]");
@@ -104,10 +124,7 @@ openButton.addEventListener("click", () => {
     modal.showModal();
 });
 
-// closeButton.addEventListener("click", () => {
-//     modal.close();
-// });
-
+// Closes modal if click outside of container
 modal.addEventListener("click", e => {
     const dialogDimensions = modal.getBoundingClientRect();
     if (
